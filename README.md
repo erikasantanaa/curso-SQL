f7107e955f022b660d07e32389b99c0093344b37
Erika Santana <erika.araujo12344@gmail.com>
2021-06-08 20:46:08 -0300

    Curso em andamento

---
+++ README.md
@@ -0,0 +1,5 @@
+<h3> Exercícios SQL Server </h3>
+
+<p>Consultar nome e sobrenome de todos os clientes cadastrados no sistema</p>
+
+<img src = "./img/ exerc01.png" width = " 500px ">
---
+++ select.sql
@@ -0,0 +1,9 @@
+/* select (selecionar)
+Comando universal; seleciona as colunas da tabela.
+from () 
+seleciona a tabela.
+*/
+
+SELECT coluna1,coluna2 FROM tabela
+
+SELECT * FROM tabela
